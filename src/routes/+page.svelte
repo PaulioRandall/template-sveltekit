fe0bdc8af57eeb5aa3161e03fa9c7845fec1fa08
Paulio <pauliorandall@gmail.com>
2025-01-09 17:26:02 +0000

    Clean up repo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,12 @@
 <script>
-	import Container from '$lib/Container.svelte'
 </script>
 
 <main>
 	<h1>&lt;page-title&gt;</h1>
-
-	<Container>
-		<Container>
-			<div class="test"> </div>
-			<div class="test"> </div>		
-		</Container>
-			<Container>
-			<div class="test"> </div>
-			<div class="test"> </div>		
-		</Container>
-	</Container>
+	<h2>A heading</h2>
+	<p>A paragraph...</p>
 </main>
 
 <style>
-	main {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.test {
-		background: lightblue;
-		height: 50px;
-	}
+	
 </style>
